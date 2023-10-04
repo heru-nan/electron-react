300d3747eb481593c9a6ac4a06abbab8d74d2e9b
heru-nan <hrwgallardo@gmail.com>
2023-10-04 18:09:19 -0300

    add build step

--- src/App.js
+++ src/App.js
@@ -15,17 +15,56 @@ export default function Game() {
   const [enemyBoard, setEnemyBoard] = useState(
     Array.from(Array(5), () => new Array(5).fill(0))
   );
-  const [ships, setShips] = useState([]);
+  const [ships, setShips] = useState({
+    s: null,
+    b: null,
+    p: null,
+  });
   const [building, setBuilding] = useState(true);
+  const [shipOrientation, setShipOrientation] = useState(1);
+  const [currentShip, setCurrentShip] = useState(null);
+
+  useEffect(() => {
+    if (ships.s?.length && ships.b?.length && ships.p?.length) {
+      setBuilding(false);
+    }
+  }, [ships]);
 
   const handleCellClick = useCallback(
     (cell, position) => {
-      if (building) {
-        board[position[0]][position[1]] = 3;
+      if (building && currentShip) {
+        if (currentShip === "p") {
+          board[position[0]][position[1]] = 3;
+          setShips({ ...ships, p: [...position, shipOrientation] });
+        } else if (shipOrientation === 1) {
+          if (currentShip === "b") {
+            board[position[0]][position[1]] = 3;
+            board[position[0]][position[1] + 1] = 3;
+            setShips({ ...ships, b: [...position, shipOrientation] });
+          }
+          if (currentShip === "s") {
+            board[position[0]][position[1]] = 3;
+            board[position[0]][position[1] + 1] = 3;
+            board[position[0]][position[1] + 2] = 3;
+            setShips({ ...ships, s: [...position, shipOrientation] });
+          }
+        } else {
+          if (currentShip === "b") {
+            board[position[0]][position[1]] = 3;
+            board[position[0] + 1][position[1]] = 3;
+            setShips({ ...ships, b: [...position, shipOrientation] });
+          }
+          if (currentShip === "s") {
+            board[position[0]][position[1]] = 3;
+            board[position[0] + 1][position[1]] = 3;
+            board[position[0] + 2][position[1]] = 3;
+            setShips({ ...ships, s: [...position, shipOrientation] });
+          }
+        }
         setBoard([...board]);
       }
     },
-    [board, building]
+    [board, building, currentShip, shipOrientation]
   );
 
   return (
@@ -41,6 +80,7 @@ export default function Game() {
           ships={ships}
           building={building}
         />
+
         {/* <Board
           title="Enemy Board"
           board={enemyBoard}
@@ -49,6 +89,27 @@ export default function Game() {
           }
         /> */}
       </div>
+      <button disabled={building} onClick={() => {}}>
+        Construir
+      </button>
+      <div className="game-board">
+        <button disabled={!building} onClick={() => setCurrentShip("s")}>
+          Build S(3)
+        </button>
+        <button disabled={!building} onClick={() => setCurrentShip("b")}>
+          Build B(2)
+        </button>
+        <button disabled={!building} onClick={() => setCurrentShip("p")}>
+          Build P(1)
+        </button>
+        <button
+          disabled={!building}
+          onClick={() => setShipOrientation(shipOrientation === 1 ? 0 : 1)}
+        >
+          Orientacion: {shipOrientation === 1 ? "Horizontal" : "Vertical"}
+        </button>
+      </div>
+
       {/* <GameInfo ships={ships} />
       {gameOver && (
         <GameOver game={state} onPlayAgain={() => handlePlayAgain()} />
